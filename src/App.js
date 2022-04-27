b2ea62e24f7e5ce30086a8cb246389c115be94ae
aryan <aryanmahalle78924@gmail.com>
2022-04-27 20:41:38 +0530

    commit commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {useState,useEffect} from 'react';
+import { NotesList } from "./components/NotesList";
+import { nanoid } from 'nanoid';
+import Search from './components/Search';
+import { Header } from './components/Header';
 
 function App() {
+  const[notes,setNotes] = useState([
+    {
+      id: nanoid(),
+      text:'This is the first note!',
+      date:'15/04/2021'
+    },
+    {
+      id: nanoid(),
+      text:'This is the second note!',
+      date:'15/04/2021'
+    },
+    {
+      id: nanoid(),
+      text:'This is the third note!',
+      date:'15/04/2021'
+    },
+    {
+      id: nanoid(),
+      text:'This is the third note!',
+      date:'30/04/2021'
+    }
+  ])
+
+  const [searchText,setSearchText]=useState('')
+
+  const[darkMode,setDarkMode] = useState(false);
+
+  useEffect(()=>{
+    const saveNotes = JSON.parse(localStorage.getItem('react-notes-app-data'))
+
+    if(saveNotes){
+      setNotes(saveNotes);
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem('react-notes-app-data',JSON.stringify(notes))
+  },[notes]);
+
+  const addNote = (text)=>{
+    const date = new Date();
+    const newNote = {
+      id:nanoid(),    
+      text:text,
+      date:date.toLocaleDateString()
+    }
+    const newNotes = [...notes,newNote]
+    setNotes(newNotes)
+  }
+
+  const deleteNote = (id)=>{
+    const newNotes = notes.filter((note)=>note.id!==id);
+    setNotes(newNotes)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList notes={notes.filter((note)=>note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote ={deleteNote} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Note.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './index.css'
+import {MdDeleteForever} from 'react-icons/md';
+
+export const Note = ({id,text,date,handleDeleteNote}) => {
+  return (
+    <div className='note'>
+        <span>{text}</span>
+        <div className='note-footer'>
+            <small>{date}</small>
+            <MdDeleteForever onClick={()=>handleDeleteNote(id)} className='delete-icon' size='1.3em' />
+        </div>
+    </div>
+  )
+}
